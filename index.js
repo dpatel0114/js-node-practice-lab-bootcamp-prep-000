1a94f643405c5d97842eb92f6710188dd9e12deb
Dhara Patel <1991dharapatel@gmail.com>
2019-02-19 04:38:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Dhara"
-var height = ""
+var height = "63"
 
 console.log("Joe")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height)+1)
